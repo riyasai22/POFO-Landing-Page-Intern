98dc8a7e87f845ed7ada6e4475cad22ff0f8fe99
riyasai22 <80235375+riyasai22@users.noreply.github.com>
2022-06-22 23:31:30 +0530

    Updates

---
+++ precache-manifest.6a1058dacaf8cf3682a355f5b84c611c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b437c4524c775703b0a5",
+    "url": "/POFO-Landing-Page-Intern/static/css/main.67c7d05d.chunk.css"
+  },
+  {
+    "revision": "b437c4524c775703b0a5",
+    "url": "/POFO-Landing-Page-Intern/static/js/main.b437c452.chunk.js"
+  },
+  {
+    "revision": "8afb4727ff329df11540",
+    "url": "/POFO-Landing-Page-Intern/static/css/1.879bf734.chunk.css"
+  },
+  {
+    "revision": "8afb4727ff329df11540",
+    "url": "/POFO-Landing-Page-Intern/static/js/1.8afb4727.chunk.js"
+  },
+  {
+    "revision": "5583bd1792d2a35fd69f",
+    "url": "/POFO-Landing-Page-Intern/static/js/runtime~main.5583bd17.js"
+  },
+  {
+    "revision": "fee0e00fce52c43991a136926ff49a60",
+    "url": "/POFO-Landing-Page-Intern/static/media/slider-img1.fee0e00f.jpg"
+  },
+  {
+    "revision": "1a9f9ac611551518d24746ae7835bcbd",
+    "url": "/POFO-Landing-Page-Intern/static/media/slider-img2.1a9f9ac6.jpg"
+  },
+  {
+    "revision": "388791d97045c42384306a678f9d58b8",
+    "url": "/POFO-Landing-Page-Intern/static/media/slider-img3.388791d9.jpg"
+  },
+  {
+    "revision": "1a44237aa9abaacd68230162a349b36c",
+    "url": "/POFO-Landing-Page-Intern/static/media/lady-side.1a44237a.jpg"
+  },
+  {
+    "revision": "ab46a3f1a31508d1e9ee9ae59e8f0fdf",
+    "url": "/POFO-Landing-Page-Intern/static/media/man-side.ab46a3f1.jpg"
+  },
+  {
+    "revision": "74aed31472d1a03436f2d2cef159c947",
+    "url": "/POFO-Landing-Page-Intern/static/media/background.74aed314.jpg"
+  },
+  {
+    "revision": "6274fcf9907884a1e259f26878c91d84",
+    "url": "/POFO-Landing-Page-Intern/static/media/service.6274fcf9.jpg"
+  },
+  {
+    "revision": "1290a0f16432da33558a9565c6d30b36",
+    "url": "/POFO-Landing-Page-Intern/static/media/ppl.1290a0f1.jpg"
+  },
+  {
+    "revision": "338716ac46531305e81582f1c217c99f",
+    "url": "/POFO-Landing-Page-Intern/static/media/man-lady.338716ac.jpg"
+  },
+  {
+    "revision": "db449f9fb9ce5b786963b2e4c35d271b",
+    "url": "/POFO-Landing-Page-Intern/static/media/banner-bg.db449f9f.jpg"
+  },
+  {
+    "revision": "e21f03e04b2bab1ea6e495aeae74ebec",
+    "url": "/POFO-Landing-Page-Intern/index.html"
+  }
+];
